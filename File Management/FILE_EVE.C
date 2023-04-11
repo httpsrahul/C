ea19657bbe822f66c5d93b0da34552cd57ab55bd
Rahul Baraiya <121658182+httpsrahul@users.noreply.github.com>
2023-04-11 20:04:24 +0530

    Add files via upload

---
+++ File Management/FILE_EVE.C
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#define SZ 10
+void main()
+{
+	int i,arr[SZ];
+	FILE *fp,*fp1,*fp2;
+	fp=fopen("AllNUMBER.txt","w");
+	fp1=fopen("EVEN_NUM.txt","w");
+	fprintf(fp1,"\n EVEN NUMBERS : ");
+	fp2=fopen("ODD_NUM.txt","w");
+	fprintf(fp2,"\n ODD NUMBERS : ");
+	printf("\n ENTER 10 INTEGERS ");
+	for(i=0;i<SZ;i++)
+	{
+	    scanf("%d",&arr[i]);
+	    fprintf(fp,"\n %d",arr[i]);
+	    if(arr[i]%2==0)
+	    {
+		fprintf(fp1,"%d,",arr[i]);
+	    }
+	    else
+	    {
+		fprintf(fp2,"%d,",arr[i]);
+	    }
+	}
+	fclose(fp);
+	fclose(fp1);
+	fclose(fp2);
+	getch();
+}
+
---
+++ File Management/FILE_PRI.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#define SZ 10
+int isPrime(int num)
+{
+	int i;
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+			break;
+	}
+	return (num==i) ? 1: 0;
+}
+void main()
+{
+	FILE *all,*prime1,*prime0;
+	int num,i,j,p=0,q=0,n;
+	clrscr();
+	all=fopen("allNUM.txt","w");
+	printf("\n ENTER NUMBER: ");
+	fprintf(all,"ENTERED NUMBER : ");
+	prime1=fopen("primeNUM.txt","w");
+	prime0=fopen("notprimeNUM.txt","w");
+	fprintf(prime1,"PRIME NUMBERS : ");
+	fprintf(prime0,"NOT PRIME NUMBER : ");
+	for(i=0;i<SZ;i++)
+	{
+		scanf("%d",&num);
+		fprintf(all,"%d,",num);
+		if(isPrime(num))
+		{
+			fprintf(prime1,"%d,",num);
+		}
+		else
+		{
+			fprintf(prime0,"%d,",num);
+		}
+	}
+	fclose(all);
+	fclose(prime1);
+	fclose(prime0);
+	getch();
+}
